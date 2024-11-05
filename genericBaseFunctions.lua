00196eb8e2f43e1b9cfe0b942614877b3c6b9fe1
WannaCry <alex.daniellw@gmail.com>
2024-11-05 19:40:58 +0100

    added randomString, made compatible with exploits without getgenv

--- genericBaseFunctions.lua
+++ genericBaseFunctions.lua
@@ -1,3 +1,4 @@
+getgenv = getgenv or function() return _G end
 getgenv().deepCopy = function(original: Table)
 	local copy = {}
 	for k, v in pairs(original) do
@@ -21,4 +22,13 @@ getgenv().notif = function(text: string, duration: number, title: string)
         Text = text,
         Duration = duration
     })
+end
+
+getgenv().randomString = function() -- yes this is from iy
+	local length = math.random(10,20)
+	local array = {}
+	for i = 1, length do
+		array[i] = string.char(math.random(32, 126))
+	end
+	return table.concat(array)
 end
