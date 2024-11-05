22a805c627c35575e570653f5c19547f7d629fa8
WannaCry <alex.daniellw@gmail.com>
2024-11-05 15:28:18 +0100

    Fixed Loader

--- LoadAllUtils.lua
+++ LoadAllUtils.lua
@@ -1,6 +1,7 @@
 local basePath = "https://raw.githubusercontent.com/WannaCry1LoL/CatnipUtils/refs/heads/main/"
 local scriptsToLoad = {
     "GoodSignal/goodsignal.lua",
+    "genericBaseFunctions.lua"
 }
 
 for i,v in ipairs(scriptsToLoad) do
